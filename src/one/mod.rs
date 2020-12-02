1b85289347553bcc20e4e87cf97039e60ac6d4f4
Jorge Gallegos <kad@blegh.net>
2020-12-01 18:15:49 -0600

    Day 1 part 2

--- src/one/mod.rs
+++ src/one/mod.rs
@@ -3,36 +3,58 @@ use std::io::{self, BufRead};
 use std::num::ParseIntError;
 use std::path::{Path, PathBuf};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Numbers {
     one: i32,
     two: i32,
     total: i32,
+    three: i32,
     product: i32,
 }
 
 pub fn cmd(path: PathBuf) -> Result<(), ParseIntError> {
     let numbers = read_numbers(path);
-    let mut nums = Numbers {
+    let mut nums1 = Numbers {
         one: 0,
         two: 0,
+        three: 0,
         total: 0,
         product: 0,
     };
+    let mut nums2 = nums1.clone();
 
-    for idx in 0..numbers.len() {
-        let this = numbers[idx];
-        let rest = &numbers[idx + 1..numbers.len()];
-        for (_, item) in rest.iter().enumerate() {
-            if this + item == 2020 {
-                nums.one = this;
-                nums.two = *item;
-                nums.total = this + *item;
-                nums.product = this * *item;
+    for idx1 in 0..numbers.len() {
+        let num1 = numbers[idx1];
+        let rest = &numbers[idx1 + 1..numbers.len()];
+        for (idx2, num2) in rest.iter().enumerate() {
+            let rest2 = &numbers[idx2 + 1..numbers.len()];
+            for (_, num3) in rest2.iter().enumerate() {
+                if num1 + num2 + num3 == 2020 {
+                    nums2.one = num1;
+                    nums2.two = *num2;
+                    nums2.three = *num3;
+                    nums2.total = num1 + *num2 + *num3;
+                    nums2.product = num1 * *num2 * *num3;
+                    break;
+                }
+            }
+            if num1 + num2 == 2020 {
+                nums1.one = num1;
+                nums1.two = *num2;
+                nums1.total = num1 + *num2;
+                nums1.product = num1 * *num2;
+            }
+            if nums1.total != 0 && nums2.total != 0 {
+                break;
             }
         }
+
+        if nums1.total != 0 && nums2.total != 0 {
+            break;
+        }
     }
-    println!("{:?}", nums);
+    println!("{:?}", nums1);
+    println!("{:?}", nums2);
 
     Ok(())
 }
